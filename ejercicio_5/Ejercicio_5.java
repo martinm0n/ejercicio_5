795aae5ced497b91a16735771d4a2ef7395d928d
martinm0n <mmontealegrec@unal.edu.co>
2023-03-04 09:47:46 -0500

    ejercicio:5

---
+++ ejercicio_5/Ejercicio_5.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.ejercicio_5;
+
+
+public class Ejercicio_5 {
+
+    public static void main(String[] args) {
+        float SUMA,X,Y;
+        SUMA=0;
+        X=20;
+        Y=40;
+        SUMA = SUMA + X;
+        X = X + Y * 2;
+        SUMA = SUMA + X / Y;
+        System.out.println("El valor de la suma es " + SUMA);
+    }
+}
